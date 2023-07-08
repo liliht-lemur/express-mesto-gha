87f096baf37fbf6215e7aa132b9ec39bc662fb3d
Liliht <liliht@MacBook-Pro-Liliht.local>
2023-07-08 23:09:43 +0400

    debug

--- app.js
+++ app.js
@@ -11,11 +11,8 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {

4da151a1a11998188deb6c88ce63d93157da8e19
Liliht <liliht@MacBook-Pro-Liliht.local>
2023-07-29 22:26:36 +0400

    cors turn-off

--- app.js
+++ app.js
@@ -33,10 +33,10 @@ app.use((req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  res.header('Access-Control-Allow-Origin', origin);
+  // if (allowedCors.includes(origin)) {
+  //   res.header('Access-Control-Allow-Origin', origin);
+  // }
+  res.header('Access-Control-Allow-Origin', '*');
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
